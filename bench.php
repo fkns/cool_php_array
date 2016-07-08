fdce40c9aadcb7de1803ffdae16a5d4bdf0bb430
Yuta Fukunishi <fukunishi@fullspeed.co.jp>
2016-07-08 15:21:01 +0900

    add array_hoge to diff

--- bench.php
+++ bench.php
@@ -11,7 +11,7 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 use Underscore\Types\Arrays as _;
 use iter\fn;
 
-const LOOP = 10;
+const LOOP = 100;
 const RANGE_MAX = 10000;
 
 function bench($name, callable $fn)
@@ -24,51 +24,6 @@ function bench($name, callable $fn)
     echo microtime(true) - $start, PHP_EOL;
 }
 
-bench('nikic/iter', function () {
-    $range = iter\range(0, RANGE_MAX);
-    $filtered = iter\filter(function ($_) {
-        return $_ % 2 === 0;
-    }, $range);
-    $mapped = iter\map(fn\operator('*', 2), $filtered);
-    $filtered2 = iter\filter(function ($_) {
-        return $_ > 20;
-    }, $mapped);
-    $result = iter\reduce(function ($carry, $item) {
-        return $carry + $item;
-    }, $filtered2);
-
-    echo $result, PHP_EOL;
-});
-
-bench('underscore', function () {
-    $result = (new _(_::range(0, RANGE_MAX)))->filter(function ($_) {
-        return $_ % 2 === 0;
-    })->each(function ($_) {
-        return $_ * 2;
-    })->filter(function ($_) {
-        return $_ > 20;
-    })->reduce(function ($carry, $item) {
-        return $carry + $item;
-    });
-
-    echo $result, PHP_EOL;
-});
-
-bench('Ginq', function () {
-    $result = Ginq::range(0, RANGE_MAX)
-        ->where(function ($_) {
-            return $_ % 2 === 0;
-        })
-        ->select(function ($_) {
-            return $_ * 2;
-        })
-        ->where(function ($_) {
-            return $_ > 20;
-        })->sum();;
-
-    echo $result, PHP_EOL;
-});
-
 bench('foreach', function () {
     $result = 0;
     $array = range(0, RANGE_MAX);
@@ -79,7 +34,7 @@ bench('foreach', function () {
         $result += $v;
     }
 
-    echo $result, PHP_EOL;
+    return $result;
 });
 
 bench('for', function () {
@@ -93,5 +48,68 @@ bench('for', function () {
         $result += $v;
     }
 
-    echo $result, PHP_EOL;
+    return $result;
+});
+
+bench('array_hoge', function () {
+    $result = array_reduce(array_filter(array_map(function ($value) {
+        return $value * 2;
+    }, array_filter(range(0, RANGE_MAX), function ($value) {
+        return $value % 2 === 0;
+    })), function ($value) {
+        return $value > 20;
+    }), function ($carry, $item) {
+        return $carry + $item;
+    });
+
+    return $result;
+});
+
+bench('underscore', function () {
+    $result = (new _(_::range(0, RANGE_MAX)))->filter(function ($value) {
+        return $value % 2 === 0;
+    })->each(function ($value) {
+        return $value * 2;
+    })->filter(function ($value) {
+        return $value > 20;
+    })->reduce(function ($carry, $item) {
+        return $carry + $item;
+    });
+
+    return $result;
+});
+
+bench('nikic/iter', function () {
+    $range = iter\range(0, RANGE_MAX);
+
+    $filtered = iter\filter(function ($value) {
+        return $value % 2 === 0;
+    }, $range);
+
+    $mapped = iter\map(fn\operator('*', 2), $filtered);
+
+    $filtered2 = iter\filter(function ($value) {
+        return $value > 20;
+    }, $mapped);
+
+    $result = iter\reduce(function ($carry, $item) {
+        return $carry + $item;
+    }, $filtered2);
+
+    return $result;
+});
+
+bench('Ginq', function () {
+    $result = Ginq::range(0, RANGE_MAX)
+        ->where(function ($value) {
+            return $value % 2 === 0;
+        })
+        ->select(function ($value) {
+            return $value * 2;
+        })
+        ->where(function ($value) {
+            return $value > 20;
+        })->sum();;
+
+    return $result;
 });
